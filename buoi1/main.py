97f7698cf4e83c01f4e57d64d9c9548d25bf46aa
phi <phinguyen2413@gmail.com>
2022-10-29 19:40:35 +0700

    sum two valuable

--- buoi1/main.py
+++ buoi1/main.py
@@ -2,3 +2,9 @@ print('Hello')
 #commit source code into repository
 
 #ton tran edit
+def chuoi(a,b):
+    s=[]
+    for i in range(0,3):
+        s.append(a[i]+b[i])
+    print(s)
+chuoi([1,2,3],[3,1,2])
